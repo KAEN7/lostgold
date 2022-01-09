d1d2b49191487b61e4f73577741db2e61f8cb6bd
kaen <kusdsuna@naver.com>
2022-01-09 17:44:47 +0900

    Add: cached test push

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+function MyApp({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />;
+}
+
+export default MyApp;
