a4d225fb7d79e7c372ffda6bd63f03e5ffa341e7
kaen <kusdsuna@naver.com>
2022-02-02 12:28:44 +0900

    Fix: solved conflict

--- redux/index.tsx
+++ redux/index.tsx
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-// redux
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { createAction } from "redux-actions";
-// import promiseMiddlerware from "redux-promise";
-
-import user from "./modules/user";
-
-// middlewares
-
-// redux router
-import { createBrowserHistory } from "history";
-export const history = createBrowserHistory();
-
-// combineReducers
-const appReducer = combineReducers({
-=======
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { HYDRATE, createWrapper } from "next-redux-wrapper";
 import user from "./modules/user";
@@ -25,7 +7,6 @@ import user from "./modules/user";
 ? 현재는 todo밖에 없지만, 다른 reducer가 추가될 상황을 가정하여 개방하는 의미도 있다.
 */
 const rootReducer = combineReducers({
->>>>>>> dev
 	user,
 });
 
@@ -45,11 +26,6 @@ const reducer = (state: any, action: any) => {
 //? store type
 export type RootState = ReturnType<typeof rootReducer>;
 
-<<<<<<< HEAD
-const rootReducer = (state: any, action: any) => {
-	if (action.type === "root/RESET_REDUCER") {
-		return appReducer(undefined, action);
-=======
 //? middleware 적용을 위한 store enhancer
 //? 리덕스 미들웨어는 액션이 디스패치 되어 리듀서에서 처리하기 전에 사전에 지정된 작업들을 의미한다.
 //? 리덕스 데브툴즈 확장 프로그램을 사용하기 위해 미들웨어에 리덕스 데브툴즈를 사용하도록 하는 코드.
@@ -57,27 +33,12 @@ const bindMiddleware = (middleware: any) => {
 	if (process.env.NODE_ENV !== "production") {
 		const { composeWithDevTools } = require("redux-devtools-extension");
 		return composeWithDevTools(applyMiddleware(...middleware));
->>>>>>> dev
 	}
 	return applyMiddleware(...middleware);
 };
 
-<<<<<<< HEAD
-const env = process.env.REACT_APP_NODE_ENV;
-// const middlewares = [thunk.withExtraArgument({ history })];
-
-// const enhancer =
-// 	env === "development"
-// 		? composeWithDevTools(applyMiddleware(...middlewares))
-// 		: compose(applyMiddleware(...middlewares));
-
-// const store = createStore(rootReducer, enhancer);
-const store = createStore(rootReducer);
-export default store;
-=======
 const initStore = () => {
 	return createStore(reducer, bindMiddleware([]));
 };
 
 export const wrapper = createWrapper(initStore);
->>>>>>> dev
