72b199b78a6daa2a5a61320c5b561cc60a0b2f8c
kaen <kusdsuna@naver.com>
2022-02-02 15:09:02 +0900

    Del: cached

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/gold":["static\u002Fchunks\u002Fpages\u002Fgold.js"],"/home":["static\u002Fchunks\u002Fpages\u002Fhome.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fgold","\u002Fhome"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/gold":["static\u002Fchunks\u002Fpages\u002Fgold.js"],"/setting":["static\u002Fchunks\u002Fpages\u002Fsetting.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fgold","\u002Fsetting"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/gold":["static\u002Fchunks\u002Fpages\u002Fgold.js"],"/home":["static\u002Fchunks\u002Fpages\u002Fhome.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fgold","\u002Fhome"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> main
->>>>>>> dev
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/gold":["static\u002Fchunks\u002Fpages\u002Fgold.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fgold"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
