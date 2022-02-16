3e9b9ebf4a6083be921161c1098854015577d3a5
kaen <kusdsuna@naver.com>
2022-02-16 23:06:13 +0900

    Add: setting 페이지 Header 추가

--- pages/setting.tsx
+++ pages/setting.tsx
@@ -32,29 +32,32 @@ const SettingRow = styled.div`
 
 function Setting() {
 	return (
-		<SettingSection>
-			{/* 각 재료들의 판매 가격을 적어주세요 */}
-			<SettingBox>
-				<SettingRow>
-					<>명파</>
-					<>20</>
-				</SettingRow>
-			</SettingBox>
+		<>
+			<Header title="setting" />
+			<SettingSection>
+				{/* 각 재료들의 판매 가격을 적어주세요 */}
+				<SettingBox>
+					<SettingRow>
+						<>명파</>
+						<>20</>
+					</SettingRow>
+				</SettingBox>
 
-			{/* 해당 캐릭터의 휴게 기준 2수 재료 수급개수를 적어주세요 */}
-			<SettingBox>
-				<SettingRow>
-					<>캐릭명</>
-					<>레이드</>
-					<>명파</>
-					<>개수</>
-					<>파괴석</>
-					<>개수</>
-				</SettingRow>
-			</SettingBox>
+				{/* 해당 캐릭터의 휴게 기준 2수 재료 수급개수를 적어주세요 */}
+				<SettingBox>
+					<SettingRow>
+						<>캐릭명</>
+						<>레이드</>
+						<>명파</>
+						<>개수</>
+						<>파괴석</>
+						<>개수</>
+					</SettingRow>
+				</SettingBox>
 
-			<button>저장</button>
-		</SettingSection>
+				<button>저장</button>
+			</SettingSection>
+		</>
 	);
 }
 
