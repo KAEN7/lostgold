889ec8a685205498b3a727416bb943fef17b11fb
kaen <kusdsuna@naver.com>
2022-02-24 16:50:55 +0900

    Add: Home 이미지 및 애니메이션 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,36 @@
 // 홈 메인화면
 
 import type { NextPage } from "next";
+import Image from "next/image";
 import { useEffect } from "react";
 import styled from "styled-components";
 import Header from "../components/Header";
-import { pageDefault, scrollBar } from "../styles/theme";
+import { flexCenterDir, pageDefault, fadeIn } from "../styles/theme";
 
 const HomeSection = styled.section`
 	${pageDefault}
+`;
+
+const HomeTitle = styled.div`
+	${flexCenterDir}
+	${fadeIn}
+	
+	margin-top: 15rem;
 `;
 
 const Index: NextPage = () => {
 	return (
 		<HomeSection>
 			<Header title="home" />
-			<h2>내 한달 골드 수입을 한눈에 확인해보세요!</h2>
+			<HomeTitle>
+				<Image
+					className="titleImg"
+					src="/image/LostGold.png"
+					width={400}
+					height={140}
+				/>
+				<h2>내 한달 골드 수입을 한눈에 확인해보세요</h2>
+			</HomeTitle>
 		</HomeSection>
 	);
 };
