32cb967091c6ee772b39bda409a19515601b4e1f
kaen <kusdsuna@naver.com>
2022-02-28 17:00:20 +0900

    Fix: 레이드 정보 컴포넌트화

--- pages/setting.tsx
+++ pages/setting.tsx
@@ -5,19 +5,11 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import ListBox from "../components/atoms/ListBox";
 import Header from "../components/Header";
 import { flexCenter, flexCenterDir, pageDefault, color } from "../styles/theme";
-import {
-	getUsers,
-	putRaidToggle,
-	putRaidToggleOn,
-	putHonerStoneToggle,
-	putStoneToggle,
-	putHonerStoneName,
-	putStoneName,
-} from "../redux/modules/user";
+import { getUsers } from "../redux/modules/user";
 import MatarialInfo from "../components/MatarialInfo";
+import RaidInfo from "../components/RaidInfo";
 
 const SettingSection = styled.header`
 	${pageDefault}
@@ -40,15 +32,6 @@ const SettingBox = styled.div<ISettingBox>`
 	border-radius: 0.84vh;
 	background: ${color.white};
 	color: black;
-`;
-
-const SettingRow = styled.div`
-	display: flex;
-	justify-content: flex-start;
-	align-items: center;
-	color: ${color.black};
-	margin: 0.5rem;
-	flex-wrap: wrap;
 `;
 
 // 아이템 박스
@@ -83,12 +66,6 @@ const ItemBox = styled.form`
 		color: ${color.point};
 		font-weight: bold;
 	}
-`;
-
-const RestHeader = styled.span`
-	font-weight: bold;
-	padding-right: 0.5rem;
-	border-right: 0.2rem solid ${color.darkPoint};
 `;
 
 // input 들어가는 form 박스
@@ -161,106 +138,9 @@ function Setting() {
 			: localStorage.setItem("matarialList", JSON.stringify(matarialList));
 	}, []);
 
-	// 레이드 목록
-	const raidList = [
-		{ name: "우르닐" },
-		{ name: "루메루스" },
-		{ name: "빙결의 레기오로스" },
-		{ name: "베루투스" },
-		{ name: "크로마니움" },
-		{ name: "나크라세나" },
-		{ name: "홍염의 요호" },
-		{ name: "타이탈로스" },
-		{ name: "어둠의 레기오로스" },
-		{ name: "헬가이아" },
-		{ name: "칼벤투스" },
-		{ name: "아카테스" },
-		{ name: "혹한의 헬가이아" },
-		{ name: "용암 크로마니움" },
-		{ name: "레바노스" },
-		{ name: "엘버하스틱" },
-		{ name: "중갑 나크라세나" },
-		{ name: "이그렉시온" },
-		{ name: "흑야의 요호" },
-		{ name: "벨가누스" },
-		{ name: "데스칼루다" },
-		{ name: "쿤겔라니움" },
-		{ name: "칼엘리고스" },
-		{ name: "하누마탄" },
-	];
-
 	// 재료 리스트 핸들러
 	const onMatarialHandler = (data: Array<any>) => {
 		setMatarialList(data);
-	};
-
-	// toggle true 변경 핸들러
-	const onToggleHandler = (charName: string) => {
-		dispatch(putRaidToggleOn({ charName: charName, toggle: false }));
-	};
-
-	// toggle false 변경 및 리스트 팝업 관련 핸들러
-	const onTitlehandler = (name: string, charName: string) => {
-		dispatch(putRaidToggle({ charName: charName, name: name, toggle: true }));
-	};
-
-	// 재료 개수 핸들러
-	const onCountHandler = (
-		e: any,
-		name: string,
-		title: string,
-		check: boolean
-	) => {
-		e.preventDefault();
-		const gold = matarialList.find((el) => el.name === name);
-		console.log(gold, "gold");
-		if (title === "honorStone") {
-			check
-				? dispatch(
-						putHonerStoneToggle({
-							count: e.target.value,
-							name: name,
-							boolean: true,
-							gold: gold ? gold.value : 0,
-						})
-				  )
-				: dispatch(
-						putHonerStoneToggle({
-							count: e.target.value,
-							name: name,
-							boolean: false,
-							gold: gold ? gold.value : 0,
-						})
-				  );
-		} else if (title === "stone") {
-			check
-				? dispatch(
-						putStoneToggle({
-							count: e.target.value,
-							name: name,
-							boolean: true,
-							gold: gold ? gold.value : 0,
-						})
-				  )
-				: dispatch(
-						putStoneToggle({
-							count: e.target.value,
-							name: name,
-							boolean: false,
-							gold: gold ? gold.value : 0,
-						})
-				  );
-		}
-	};
-
-	// 재료명 변경 핸들러
-	const onHonorStoneTitleHandler = (name: string, key: string) => {
-		dispatch(putHonerStoneName({ name: key, stoneName: name, boolean: true }));
-	};
-
-	const onStoneTitleHandler = (name: string, key: string) => {
-		console.log(name, key);
-		dispatch(putStoneName({ name: key, stoneName: name, boolean: true }));
 	};
 
 	// 캐릭터 생성 상태값
@@ -328,79 +208,11 @@ function Setting() {
 				<br />
 
 				{/* 레이드 정보 */}
-				<h3>해당 캐릭터의 휴게 기준 재료 수급개수를 적어주세요</h3>
-				<SettingBox>
-					{userData.length === 0 ? (
-						<h3>캐릭터가 없습니다 새로 추가해보세요!</h3>
-					) : (
-						userData.map((el: any, idx: number) => (
-							<SettingRow key={`setup${idx}`}>
-								<RestHeader>{el.name}</RestHeader>
-								{el.raid.toggle ? (
-									<FormBox onClick={() => onToggleHandler(el.name)}>
-										{el.raid.name}
-									</FormBox>
-								) : (
-									<ListBox name={el.name} onTitlehandler={onTitlehandler}>
-										{raidList}
-									</ListBox>
-								)}
-								{/* 돌파석 */}
-								{el.honorStone.boolean ? (
-									<FormBox onSubmit={(e) => e.preventDefault()}>
-										<span
-											onClick={(e) =>
-												onCountHandler(e, el.name, "honorStone", false)
-											}
-										>
-											{el.honorStone.name}:
-										</span>
-
-										<input
-											type="number"
-											placeholder="개수"
-											value={el.honorStone.count}
-											onChange={(e) =>
-												onCountHandler(e, el.name, "honorStone", true)
-											}
-										/>
-									</FormBox>
-								) : (
-									<ListBox
-										name={el.name}
-										onTitlehandler={onHonorStoneTitleHandler}
-									>
-										{matarialList.slice(3)}
-									</ListBox>
-								)}
-								{/* 파괴석 */}
-								{el.stone.boolean ? (
-									<FormBox onSubmit={(e) => e.preventDefault()}>
-										<span
-											onClick={(e) =>
-												onCountHandler(e, el.name, "stone", false)
-											}
-										>
-											{el.stone.name}:
-										</span>
-										<input
-											type="number"
-											placeholder="개수"
-											value={el.stone.count}
-											onChange={(e) =>
-												onCountHandler(e, el.name, "stone", true)
-											}
-										/>
-									</FormBox>
-								) : (
-									<ListBox name={el.name} onTitlehandler={onStoneTitleHandler}>
-										{matarialList.slice(0, 3)}
-									</ListBox>
-								)}
-							</SettingRow>
-						))
-					)}
-				</SettingBox>
+				<RaidInfo
+					SettingBox={SettingBox}
+					FormBox={FormBox}
+					matarialList={matarialList}
+				/>
 				<br />
 
 				{/* 캐릭터 추가 */}
