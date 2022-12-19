b869f93b8ced2e7180e1afc7335fbfb2a3ee3ab9
kaen <kusdsuna@naver.com>
2022-12-19 10:31:12 +0900

    feat: openAPI 캐릭터 연동

---
+++ pages/api/armories.ts
@@ -0,0 +1,48 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+const armories = async (req: NextApiRequest, res: NextApiResponse) => {
+	const name: string | any = req.query.data;
+
+	const armoriesList: any = [];
+
+	const apiList = [
+		"profiles",
+		"equipment",
+		"avatars",
+		"combat-skills",
+		"engravings",
+		"cards",
+		"gems",
+		"colosseums",
+		"collectibles",
+	];
+
+	await Promise.all(
+		apiList.map(
+			async (path, idx) =>
+				await axios
+					.get(
+						`${
+							process.env.NEXT_PUBLIC_LOSTARK_OPENAPI
+						}/armories/characters/${encodeURIComponent(name)}/${path}`,
+						{
+							headers: {
+								accept: "appication/json",
+								"Content-Type": "application/json",
+								authorization: `bearer ${process.env.NEXT_PUBLIC_LOSTARK_JWT}`,
+							},
+						}
+					)
+					.then(({ data }) => (armoriesList[idx] = data))
+					.catch((err) => {
+						console.error("Characters ERROR", err);
+						res.status(404).json({ err: "Not Found" });
+					})
+		)
+	);
+
+	res.status(200).json({ data: armoriesList });
+};
+
+export default armories;
--- pages/character/index.tsx
+++ pages/character/index.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { loadingState, searchCharacterList } from "../../store";
-import { color, flexCenterDir } from "../../styles/theme";
+import { color, flexCenterDir, overflowY } from "../../styles/theme";
 
 interface charSetup {
 	CharacterClassName: string;
@@ -21,33 +21,46 @@ const Index: NextPage = () => {
 
 	console.log("searchList", searchList);
 
-	const onSubmitHandler = async (e: any, name: string) => {
+	const onDetailHandler = async (e: any, name: string) => {
 		e.preventDefault();
+		setLoading(true);
 
 		await axios
-			.get("/api/characters", { params: { data: name } })
-			.then((data) => console.log("data", data.data));
+			.get("/api/armories", { params: { data: name } })
+			.then((data) => {
+				console.log("data", data.data);
+				setLoading(false);
+			});
 	};
 
 	return (
 		<CharacterSection>
 			<CharacterList>
 				{searchList.map((char: charSetup) => (
-					<li>
-						<span>{char.ServerName}</span>
-						<span>{char.CharacterClassName}</span>
-						<span>{char.CharacterLevel}</span>
-						<span>{char.ItemMaxLevel}</span>
-						<span>{char.CharacterName}</span>
+					<li onClick={(e) => onDetailHandler(e, char.CharacterName)}>
+						<div>
+							<span>{char.ServerName}</span>
+							<span>{char.CharacterClassName}</span>
+						</div>
+
+						<span className="name">{char.CharacterName}</span>
+
+						<div>
+							<span>I.L {char.ItemMaxLevel}</span>
+							<span>C.L {char.CharacterLevel}</span>
+						</div>
 					</li>
 				))}
 			</CharacterList>
+
+			<CharacterDetail></CharacterDetail>
 		</CharacterSection>
 	);
 };
 
 const CharacterSection = styled.main`
 	${flexCenterDir}
+	${overflowY}
 
 	width: 100%;
 	height: 100%;
@@ -55,27 +68,53 @@ const CharacterSection = styled.main`
 
 const CharacterList = styled.ul`
 	display: flex;
-	justify-content: flex-start;
+	justify-content: space-around;
 	flex-wrap: wrap;
 	width: 50%;
 	padding: 1rem;
 	box-sizing: border-box;
 	background: ${color.gray};
 	border-radius: 10px;
+	margin-bottom: 3rem;
 
 	li {
 		display: flex;
 		flex-direction: column;
-		width: 8rem;
+		width: 12rem;
+		border-radius: 14px;
 		background: ${color.black};
 		margin: 1rem;
+		padding: 1rem;
 		color: ${color.white};
+		cursor: pointer;
+		box-shadow: 0px 0.5px 20px ${color.black};
+		transition: all 0.3s ease 0s;
+
+		&:hover {
+			background-color: #2ea977;
+			box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+			transform: translateY(-7px);
+		}
+
+		div {
+			display: flex;
+			justify-content: space-between;
+			width: 100%;
+		}
+
+		.name {
+			font-size: 1.1rem;
+			font-weight: bold;
+			margin-top: 1rem;
+			margin-bottom: 2rem;
+		}
 	}
 `;
 
-const DetailSection = styled.div`
+const CharacterDetail = styled.div`
 	display: flex;
 	flex-direction: column;
+	background: ${color.gray};
 `;
 
 export default Index;
