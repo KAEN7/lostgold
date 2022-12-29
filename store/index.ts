97b91f15815d972f1f64c2e716d1fb96a61563cf
kaen <kusdsuna@naver.com>
2022-12-30 00:09:34 +0900

    feat: recoil-persist 적용

--- store/index.ts
+++ store/index.ts
@@ -1,8 +1,22 @@
 import { atom, selector } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+// normal
+const { persistAtom } = recoilPersist();
+
+// sessionStorage
+// const sessionStorage =
+// 	typeof window !== "undefined" ? window.sessionStorage : undefined;
+
+// const { persistAtom } = recoilPersist({
+// 	key: "내맘대로 정하는 키 이름",
+// 	storage: sessionStorage,
+// });
 
 export const toggleAside = atom({
 	key: "toggleAside",
 	default: true,
+	effects_UNSTABLE: [persistAtom],
 });
 
 export const toggleAsideSelector = selector({
@@ -16,19 +30,23 @@ export const toggleAsideSelector = selector({
 export const nameState = atom({
 	key: "nameState",
 	default: "",
+	effects_UNSTABLE: [persistAtom],
 });
 
 export const searchCharacterList = atom({
 	key: "searchCharacterList",
 	default: [],
+	effects_UNSTABLE: [persistAtom],
 });
 
 export const armoriesState = atom({
 	key: "armoriesState",
 	default: [],
+	effects_UNSTABLE: [persistAtom],
 });
 
 export const loadingState = atom({
 	key: "loadingState",
 	default: false,
+	effects_UNSTABLE: [persistAtom],
 });
